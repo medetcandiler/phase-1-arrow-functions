2c9b2ee54379df2449a447c7bd42c69928584ea8
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-24 21:56:04 +0300

    completed assignment.

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(){
+  let a = 2000;
+  let b = 100;
+  return a / b;
+}
+
+const square = num => num * num ;
+
+square(4);
+
+const add = (a, b ) => {
+  return a + b;
+}
+
+add(4,67)
